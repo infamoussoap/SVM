47fcee29acba168616ee9b4a4c82503f4ccd4a17
infamoussoap <james.chok1000+github@gmail.com>
2021-12-03 15:49:50 +1100

    LazyKernel now moved to its own folder

--- StochasticSMO.py
+++ StochasticSMO.py
@@ -2,21 +2,7 @@ import numpy as np
 from functools import lru_cache
 import warnings
 
-
-class LazyKernel:
-    def __init__(self, x_train, kernel_function):
-        self.x_train = x_train
-        self.kernel_function = kernel_function
-
-    def __getitem__(self, slice_):
-        if isinstance(slice_, slice):
-            slice1 = slice_
-            slice2 = slice(None, None, None)
-        else:  # slice is instance of tuple
-            assert len(slice_) == 2, "Indexing only valid for 2D indexing"
-            slice1, slice2 = slice_
-
-        return self.kernel_function(self.x_train[slice1], self.x_train[slice2])
+from Kernels import LazyKernel
 
 
 class StochasticSMO:
