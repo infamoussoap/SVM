bcb2771ab26291c537fb8104aa91336c71269ac4
infamoussoap <james.chok1000+github@gmail.com>
2021-12-12 14:07:34 +1100

    CythonStochasticSMO can now be used in SVM

--- Optimizers/get_optimizer.py
+++ Optimizers/get_optimizer.py
@@ -1,10 +1,11 @@
 from .SMO import SMO
 from .StochasticSMO import StochasticSMO
 from .CythonSMO import CythonSMO
+from .CythonStochasticSMO import CythonStochasticSMO
 
 
 def get_new_instance_of_optimizer(optimizer):
-    if isinstance(optimizer, (SMO, StochasticSMO, CythonSMO)):
+    if isinstance(optimizer, (SMO, StochasticSMO, CythonSMO, CythonStochasticSMO)):
         return type(optimizer)(**optimizer.get_config())
     elif optimizer.lower() == 'stochasticsmo':
         return StochasticSMO()
@@ -12,5 +13,7 @@ def get_new_instance_of_optimizer(optimizer):
         return SMO()
     elif optimizer.lower() == 'cythonsmo':
         return CythonSMO(1.0, 1e-2, 1e-2)
+    elif optimizer.lower() == 'cythonstochasticsmo':
+        return CythonStochasticSMO()
     else:
         raise ValueError(f"{optimizer} is not a valid optimizer.")
