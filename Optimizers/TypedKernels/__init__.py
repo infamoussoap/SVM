1ce2ba8681cae27dc5bdbd42d78b7766fea967d5
infamoussoap <james.chok1000+github@gmail.com>
2021-12-15 13:15:34 +1100

    importing the new files

--- Optimizers/TypedKernels/__init__.py
+++ Optimizers/TypedKernels/__init__.py
@@ -1,9 +1,14 @@
 from .DiskKernel import DiskKernel
 from .LazyKernel import LazyKernel
 from .StockKernel import StockKernel
-from .DiskKernelV2 import DiskKernel as DiskKernelV2
 
-kernels = {"stock": StockKernel, "lazy": LazyKernel, "disk": DiskKernel}
+from .DiskKernelV2 import DiskKernel as DiskKernelV2
+from .LazyKernelV2 import LazyKernel as LazyKernelV2
+from .AdaptiveKernel import AdaptiveKernel
+
+
+kernels = {"stock": StockKernel, "lazy": LazyKernel, "disk": DiskKernel,
+           "adaptive": AdaptiveKernel, "lazyv2": LazyKernelV2, "diskv2": DiskKernelV2}
 
 
 def get_kernel(kernel_type):
